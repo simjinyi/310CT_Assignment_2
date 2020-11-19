a53d2e5c5c9d294921de958776c2f3c13f401913
simjinyi <38304422+simjinyi@users.noreply.github.com>
2020-11-19 12:22:01 +0800

    Retrieve All

--- Source Code/run.bat
+++ Source Code/run.bat
@@ -1,2 +1,2 @@
 @echo off
-java -classpath jam/jam.jar com.irs.jam.JAM "310CT- Assignment 2.jam"
+java -classpath jam/jam.jar com.irs.jam.JAM "310CT - Assignment 2.jam"
--- Source Code/run.sh
+++ Source Code/run.sh
@@ -1,1 +1,1 @@
-java -classpath jam/jam.jar com.irs.jam.JAM "310CT- Assignment 2.jam"
+java -classpath jam/jam.jar com.irs.jam.JAM "310CT - Assignment 2.jam"
